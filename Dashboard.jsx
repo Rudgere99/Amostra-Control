329711eae6e86490a67c64028aeb83144698d334
Rudgere99 <rudgeregermanos@gmail.com>
2026-06-16 09:54:28 -0300

    Refresh app visual styling

--- Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SampleBadge({ value }) {
-  return (
-    <span className={`sample ${value ? 'sample--ok' : 'sample--empty'}`}>
-      {value ? 'OK' : '--'}
-    </span>
-  )
-}
--- LoginPage.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { getStatusLabel } from '../utils/status.js'
-
-export default function StatusBadge({ status }) {
-  return <span className={`status status--${status}`}>{getStatusLabel(status)}</span>
-}
